f30b7b66dbfadf3ec99a9f71b092db191229014b
Guillaume Lours <guillaume.lours@docker.com>
2019-10-16 15:06:38 +0200

    add coordinate to beer object
    Signed-off-by: Guillaume Lours <guillaume.lours@docker.com>

--- server/src/main/kotlin/com/docker/dockerfile/bestpractices/domain/Beer.kt
+++ server/src/main/kotlin/com/docker/dockerfile/bestpractices/domain/Beer.kt
@@ -3,7 +3,8 @@ package com.docker.dockerfile.bestpractices.domain
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Beer(@Id var id: String = "",
-                @JsonProperty("name") val name: String) {
-}
+                @JsonProperty("name") val name: String,
+                var coordinates: List<Double> = emptyList())
