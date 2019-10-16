a13e7478fde23e5000aeb115778d8a5f1e460caa
Guillaume Lours <guillaume.lours@docker.com>
2019-10-16 14:37:54 +0200

    add server startup configuration & api routes
    Signed-off-by: Guillaume Lours <guillaume.lours@docker.com>

--- server/src/main/kotlin/com/docker/dockerfile/bestpractices/api/RandomApi.kt
+++ server/src/main/kotlin/com/docker/dockerfile/bestpractices/api/RandomApi.kt
@@ -1,15 +1,29 @@
 package com.docker.dockerfile.bestpractices.api
 
+import com.docker.dockerfile.bestpractices.domain.Beer
+import com.docker.dockerfile.bestpractices.storage.BeerRepository
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
-
+import java.util.*
 
 
 @RestController
-class RandomApi {
+class RandomApi(val beerRepository: BeerRepository) {
 
     @RequestMapping("/")
     fun index(): String {
         return "Hello Devoxx 👋"
+    }
+
+    @RequestMapping("/beers")
+    fun list(): List<Beer> {
+        return beerRepository.findAll()
+    }
+
+    @RequestMapping("/beer")
+    fun random(): Beer {
+        val beers =  beerRepository.findAll()
+        beers.shuffle()
+        return beers.first()
     }
 }
